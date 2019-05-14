8aa2f1f7644c07e5b9d832a44d45ee9da1c126d5
Thaissa Crato <cratothaissa34@gmail.com>
2019-05-14 18:40:32 -0300

    respJS

---
+++ respJS_001.js
@@ -0,0 +1,6 @@
+function Q2() {
+    console.log('Q2');
+    var x = Number.parseFloat(document.getElementById('b').value);
+    var y = Number.parseFloat(document.getElementById('c').value);
+    // Segue aqui seu código
+}
