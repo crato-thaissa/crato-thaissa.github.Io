eea0dff241ed152df591d14936c0b8bfb93c5251
Thaissa Crato <cratothaissa34@gmail.com>
2019-05-16 10:23:02 -0300

    respJS_001.html

--- respJS_001.js
+++ respJS_001.js
@@ -1,6 +1,41 @@
+function Q1() {
+    console.log('Q1');
+    var x = Number.parseInt(document.getElementById('x').value);
+    var y = Number.parseInt(document.getElementById('y').value);
+    // Segue aqui seu código
+    console.log ('X / Y: ' + x + ' / ' + y)
+    console.log ('Quociente = ' + Math.floor(x/y));
+console.log ('Resto = ' + (x%y));
+}
+
 function Q2() {
     console.log('Q2');
     var x = Number.parseFloat(document.getElementById('b').value);
     var y = Number.parseFloat(document.getElementById('c').value);
     // Segue aqui seu código
 }
+
+function Q3() {
+    console.log('Q3');
+    var aux = document.getElementById('profissoes');
+    var profissao = aux.options[aux.selectedIndex].text;
+    // Segue aqui seu código
+}
+
+function Q4() {
+    console.log('Q4');
+    var a_str = document.getElementById('a').value;
+    var b_str = document.getElementById('b').value;
+    var c_str = document.getElementById('c').value;
+    // Segue aqui seu código
+}
+
+function Q5() {
+    console.log('Q5');
+    var altura = Number.parseFloat(document.getElementById('altura').value);
+    var peso = Number.parseFloat(document.getElementById('peso').value);
+    var aux = document.getElementById('generos');
+    var genero = aux.options[aux.selectedIndex].text;
+    // Segue aqui seu código
+}
+
