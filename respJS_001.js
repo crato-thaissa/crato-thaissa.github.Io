e5747aeedf4194f7aafd9c8d8d1d53c30d438937
Thaissa Crato <cratothaissa34@gmail.com>
2019-05-22 21:36:08 -0300

    function

--- respJS_001.js
+++ respJS_001.js
@@ -14,8 +14,8 @@ function Q2() {
     var x = Number.parseFloat(document.getElementById('b').value);
     var y = Number.parseFloat(document.getElementById('c').value);
     // Segue aqui seu código
-    console.log('b e c = ' + x + ' / ' + y);
-    console.log('HIpotenusa = ' + Math.sqrt(Math.pow(x,2) + Math.pow(y,2)));
+    console.log('b e c = ' + ' ' + x + ' / ' + y);
+    console.log('Hipotenusa = ' + Math.sqrt(Math.pow(x,2) + Math.pow(y,2)));
 }
 
 
@@ -25,17 +25,17 @@ function Q3() {
     var profissao = aux.options[aux.selectedIndex].text;
     // Segue aqui seu código
     if (profissao .endsWith('Musicista')) {
-        console.log(profissao + ' ' + 'substantivo de ambos gêneros')
+        console.log(profissao + ' ' + 'substantivo ambos gêneros')
     } else if (profissao .endsWith('a')) {
-        console.log (profissao + ' ' + 'substantivo feminino')
+        console.log (profissao + ' ' + 'substantivo feminino');
     } else if (profissao .endsWith('o')) {
-        console.log (profissao + ' ' + 'substantivo masculino')
+        console.log (profissao + ' ' + 'substantivo masculino');
     } else if (profissao .endsWith('r')) {
-        console.log (profissao + ' ' + 'substantivo masculino')
+        console.log (profissao + ' ' + 'substantivo masculino');
     } else if (profissao .endsWith('z')) {
-        console.log(profissao + ' ' + 'substantivo femininoo')
-    } else if (profissao .endsWith('e')) {
-        console.log('substantivo de ambos gêneros')
+        console.log(profissao + ' ' + 'substantivo femininoo');
+    } else {
+        console.log('substantivo de ambos gêneros');
     }        
 }
 
@@ -43,42 +43,31 @@ function Q3() {
 function Q4() {
     console.log('Q4');
     var a_str = document.getElementById('a').value;
-    var b_str = document.getElementById('b').value;
-    var c_str = document.getElementById('c').value;
+    var b_str = document.getElementById('d').value;
+    var c_str = document.getElementById('e').value;
     // Segue aqui seu código
     if((!a_str) && (!b_str) && (!c_str)) {
         console.log('Escreva dois valores')
     }
-    else { 
-        if ((!a_str) && (!b_str)) {
+    else if ((!a_str) && (!b_str)) {
         console.log('Escreva dois valores')
         }
-    }
-    else {
-        if ((!b_str) && (!c_str)) {
+    else if ((!b_str) && (!c_str)) {
         console.log('Escreva dois valores')
         }
-    }
-    else {
-        if ((!a_str) && (!c_str)) {
+    else if ((!a_str) && (!c_str)) {
         console.log('Escreva dois valores')
         }
-    }
-    else {
-        if (!a_str) {
+    else if (!a_str) {
         console.log ('Hipotenusa = ' +  Math.sqrt(Math.pow(b_str,2) + Math.pow(c_str,2)));
         }
-    }
-    else {
-        if (!b_str) {
+    else if (!b_str) {
         console.log ('cateto oposto = ' +  Math.sqrt(Math.pow(a_str,2) - Math.pow(c_str,2)));
         }
-    }
     else {
         if (!c_str) {
         console.log ('cateto adjacente = ' +  Math.sqrt(Math.pow(a_str,2) - Math.pow(b_str,2)));
         }
-    }
 
 
     function Q5() {
@@ -90,17 +79,23 @@ function Q4() {
         // Segue aqui seu código (imc = peso/(altura+altura))
         // (imc = peso / (altura+altura))
         var imc = (peso/Math.pow(altura,2))
-        console.log('Valor IMC = ' + (peso/Math.pow(altura,2)))
-        if (imc > 18.5) {
+        console.log('Valor do seu IMC = ' + (peso/Math.pow(altura,2)))
+        if (imc < 18.5) {
             console.log('Magro(a)');
         }
-        else if (imc < 18.5, imc > 24.9) {
+        else if (imc > 18.5) {
             console.log('Sarado(a)');
         }
-        else if (imc <= 25, imc >=29.9) {
+        else if (imc < 24.9) {
+            console.log('sarado(a)');
+        }
+        else if (imc > 25) {
             console.log('Rechonchudo(a)');
         }
         else if (imc > 30) {
+            console.log('rechonchudo(a)');
+        }
+        else if (imc < 30) {
             console.log('Obeso(a),' + ' ' + 'consulte um médico e cuide da tua alimentação');
         }
     }
