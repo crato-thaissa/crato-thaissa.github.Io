39cc9dd2c5711b8bad01f07bdc3fa125d2a442d6
Thaissa Crato <cratothaissa34@gmail.com>
2019-05-29 23:31:07 -0300

    004js

--- respJS_004.js
+++ respJS_004.js
@@ -1,6 +1,21 @@
 <!DOCTYPE html>
 <html lang="pt-BR">
     <head>
-
+    <meta charset="UTF-8">
+    <style>
+    .rodape {
+            outline: 1px solid rgb(0, 0, 0)50, 246, 246);
+            outline-offset: 5px;
+            width: 100%;
+            text-align: center;
+            font-size: 0.9em; /* 1em é o tamanho normal da fonte */
+        }
+    </style>
     </head>
+    <body>
+        <h1> Volte mais Tarde ... ;) </h1>
+        <div class="rodape">
+                <p>&copy; 2019 crato-thaissa.github.io </p>
+        </div>
+    </body>
 </html>
