df4861ca40f6a177774c9bac14eff886447381c9
Thaissa Crato <cratothaissa34@gmail.com>
2019-06-13 09:54:46 -0300

    init

---
+++ python/continua.py
@@ -0,0 +1,14 @@
+# Tipos de dados
+a = None
+if a is None:
+    print ("Nada!")
+
+b = list (i for i in range(5))
+b = None
+if not (b is None):
+    print (b)
+
+lista = [2,3,4,5]
+a = tuple()
+tupla = (2,3,4,5)
+print (lista, tupla)
