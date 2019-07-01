39c3abd95861a879c3665eb9005272aa54bdf920
Thaissa Crato <cratothaissa34@gmail.com>
2019-07-01 20:20:15 -0300

    py

--- python/bibliotecas.py
+++ python/bibliotecas.py
@@ -1,11 +1,12 @@
 import math
 import random as rnd
+from os import path
 from hashlib import sha256
 import boba
 
 alpha = list ((a/10)*2*math.pi for a in range (11))
 print (alpha)
-print (list (map(math.cos,alpha)))
+print (list (map(math.cos, alpha)))
 print (math.sin(2*math.pi))
 
 def dado():
